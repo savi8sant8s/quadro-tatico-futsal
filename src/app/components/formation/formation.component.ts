2ce8f6786fb8fbe40c81826f61adcb275e8dd2a8
Sávio Santos <saviosa08@gmail.com>
2022-04-20 17:04:12 -0300

    Created formation component and logic.

--- src/app/components/formation/formation.component.ts
+++ src/app/components/formation/formation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { FormationType } from 'src/app/types/formation.type';
 
 @Component({
   selector: 'app-formation',
@@ -6,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formation.component.scss'],
 })
 export class FormationComponent implements OnInit {
+  formations: Array<FormationType> = new Array<FormationType>();
+  formationSelected = '1-2-2';
 
-  constructor() { }
+  constructor(
+    private modal: ModalController
+  ) {
+    this.formations.push({name: '1-2-2',   imageUrl: 'assets/1-2-2.svg'});
+    this.formations.push({name: '1-3-1',   imageUrl: 'assets/1-3-1.svg'});
+    this.formations.push({name: '1-1-2-1',   imageUrl: 'assets/1-1-2-1.svg'});
+    this.formations.push({name: '1-1-1-2',   imageUrl: 'assets/1-1-1-2.svg'});
+   }
 
   ngOnInit() {}
 
+  async closeModal(){
+    await this.modal.dismiss();
+  }
+
+  selectFormation(name){
+    this.formationSelected = name;
+  }
+
+  async confirmOption(){
+    await this.modal.dismiss(this.formationSelected);
+  }
 }
