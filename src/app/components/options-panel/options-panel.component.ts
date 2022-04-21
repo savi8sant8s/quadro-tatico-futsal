dd16873d3c3910cb18323bd13a90528fa09db3be
Sávio Santos <saviosa08@gmail.com>
2022-04-21 07:13:45 -0300

    Refat layout items

--- src/app/components/options-panel/options-panel.component.ts
+++ src/app/components/options-panel/options-panel.component.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { OptionType } from '../../types/option.type';
 import { FormationComponent } from '../formation/formation.component';
 import { SettingsComponent } from '../settings/settings.component';
-import { SubstitutionComponent } from '../substitution/substitution.component';
 
 @Component({
   selector: 'app-options-panel',
   templateUrl: './options-panel.component.html',
   styleUrls: ['./options-panel.component.scss'],
 })
-export class OptionsPanelComponent {
+export class OptionsPanelComponent implements OnInit{
+  @Input() playerOptions = false;
   options: Array<OptionType> = new Array<OptionType>();
 
   constructor(
     private modalCtrl: ModalController
   ) {
-    this.options.push({imageUrl: 'assets/formacao.svg', component: FormationComponent});
-    this.options.push({imageUrl: 'assets/substituicao.svg', component: SubstitutionComponent});
-    this.options.push({imageUrl: 'assets/ajustes.svg', component: SettingsComponent});
+  }
+
+   ngOnInit() {
+    if (this.playerOptions) {
+      this.options.push({imageUrl: 'assets/formacao.svg', component: FormationComponent});
+      this.options.push({imageUrl: 'assets/ajustes.svg', component: SettingsComponent});
+    } else {
+      this.options.push({imageUrl: 'assets/salvar.svg', component: null});
+      this.options.push({imageUrl: 'assets/mais.svg', component: null});
+    }
    }
 
    async openModal(component: any) {
