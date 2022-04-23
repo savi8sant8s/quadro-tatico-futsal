9edc810269a02e64494271558c20e6f13bb05123
Sávio Santos <saviosa08@gmail.com>
2022-04-23 09:44:31 -0300

    Refat layout and logic

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'quadro-tatico-futsal',
+  appId: 'com.prancheta.quadrotaticodefutsal',
+  appName: 'Quadro Tático de Futsal',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
 import { PreferencesService } from './services/preferences/preferences.service';
-
+import { App } from '@capacitor/app';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -12,10 +12,21 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private preferences: PreferencesService,
+    private modalCtrl: ModalController
 
   ) {
     this.platform.ready().then(async () => {
       await this.preferences.setDefaultPreferences();
+      this.backButtonEvent();
+    });
+  }
+
+  backButtonEvent() {
+    App.addListener('backButton', async () => {
+      const openedModal = await this.modalCtrl.getTop();
+      if (!openedModal) {
+        App.exitApp();
+      }
     });
   }
 }
