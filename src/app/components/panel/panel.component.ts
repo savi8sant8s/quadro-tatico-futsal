d2ffcf2a31fa2ed93eaa0722e486d82e71a00570
Savio Santos <saviosa08@gmail.com>
2022-06-29 20:24:37 -0300

    Added publication images and youtube video.

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -4,7 +4,6 @@ import { ModalController } from '@ionic/angular';
 
 import { CourtThemeComponent, FormationComponent, MoreComponent, PlayerThemeComponent } from '../../components';
 import { Option } from '../../types';
-import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
@@ -19,31 +18,13 @@ export class PanelComponent implements OnInit {
     this.options.push({ name: 'easel-outline', component: FormationComponent });
     this.options.push({ name: 'person-outline', component: PlayerThemeComponent });
     this.options.push({ name: 'color-palette-outline', component: CourtThemeComponent });
-    this.options.push({ name: 'save-outline', component: 'save-image' });
     this.options.push({ name: 'ellipsis-horizontal-circle-outline', component: MoreComponent });
   }
 
   async onOpenModal(component: any): Promise<void> {
-    if (component === 'save-image'){
-      this.saveImage();
-    } else {
-      const modal = await this.modalCtrl.create({
-        component,
-      });
-      modal.present();
-    }
-  }
-
-  saveImage(): void {
-    const court = document.getElementById('court');
-    html2canvas(court).then((canvas)=> {
-      canvas.toBlob((blob) => {
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = 'quadro-tático-de-futsal.png';
-        link.click();
-      });
+    const modal = await this.modalCtrl.create({
+      component,
     });
+    modal.present();
   }
 }
